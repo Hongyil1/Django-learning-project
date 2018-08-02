7742ccf2318e95f02d0d614f248746bb0a665f69
Hongyil1 <hylxmu@163.com>
2018-08-02 17:15:10 +1000

    paginator

--- mysite_env/mysite/blog/views.py
+++ mysite_env/mysite/blog/views.py
@@ -5,14 +5,13 @@ from .models import Blog, BlogType
 # Create your views here.
 # 返回列表
 def blog_list(request):
-
-    # blogs_all_list = Blog.objects.all()
-    # paginator = Paginator(blogs_all_list, 10) # 10 articles per page
-    # page_num = request.Get.get("page", 1) # get page number (Get requets)
-    # page_of_blogs = paginator.get_page(page_num) # return 1 when invalid input
+    page_num = request.GET.get('page', 1) # 获取页码参数(GET请求)
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10) # 10 articles per page
+    page_of_blogs = paginator.get_page(page_num) # return 1 when invalid input
 
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     context['blogs_count'] = Blog.objects.all().count()
     return render_to_response('blog/blog_list.html', context=context)
